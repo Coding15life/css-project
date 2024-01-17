1146101bb88aabd86939518cd04a22a01a39f539
Javier Lim <s10241539@connect.np.edu.sg>
2024-01-17 19:07:26 +0800

    feat: interactive images

--- src/app/category/popular-sports.jsx
+++ src/app/category/popular-sports.jsx
@@ -2,27 +2,35 @@ export default function PopularSports() {
     return (
         <div className="popular-sports">
             <h1 className="font-bold underline">Most Popular Sports</h1>
+            <div className="sports-image">
+                <img 
+                    src="/football.png" 
+                    alt="Mbappe playing football"
+                    width="700px"
+                    height="500px"
+                />
+                <div className="sports-page">
+                    <a href="#">See More</a>
+                </div>
+            </div>
 
-            <img 
-                src="/football.png" 
-                alt="Mbappe playing football"
-                width="700px"
-                height="500px"
-            />
+            <div className="sports-image">
+                <img
+                    src="/cricket.png"
+                    alt="2 woman playing cricket" 
+                    width="700px"
+                    height="500px"
+                />
+            </div>
 
-            <img
-                src="/cricket.png"
-                alt="2 woman playing cricket" 
-                width="700px"
-                height="500px"
-            />
-
-            <img
-                src="/hockey.png"
-                alt="6 man playing hockey"
-                width="700px"
-                height="500px"
-            />
+            <div className="sports-image">
+                <img
+                    src="/hockey.png"
+                    alt="6 man playing hockey"
+                    width="700px"
+                    height="500px"
+                />
+            </div>
         </div>
     );
 }
