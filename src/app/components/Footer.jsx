ef9062010392e91881538be1d0f4aa7e4e72e623
Bjng05 <133935568+Bjng05@users.noreply.github.com>
2024-02-06 10:56:10 +0800

    Homepage Components (benedict stuff)

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "src/app/globals.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2024 Your Website Name. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
