999bf7a1d1f02bf429311ab5202595335e3c2165
Bjng05 <133935568+Bjng05@users.noreply.github.com>
2024-02-07 21:17:06 +0800

    Update CarouselSection.js

--- src/components/CarouselSection.js
+++ src/components/CarouselSection.js
@@ -4,6 +4,18 @@ import Carousel from 'react-bootstrap/Carousel';
 function CarouselSection() {
   return (
     <div>
+      <section className="w-full py-12 md:py-24 lg:py-32">
+      <div className="container px-4 md:px-6">
+        <div className="flex flex-col items-center space-y-4 text-center">
+        <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+          Welcome to the Sports Sanctuary
+        </h1>
+          <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+            Explore the world of sports with us. Find out more about recent games, headlines and anything sports related.
+          </p>
+        </div>
+      </div>
+      </section>
       <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
         <div className="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
