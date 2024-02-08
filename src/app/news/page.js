8c3ce4f2fa6d863a446a6e53f5fa5460f7b36c5c
Bjng05 <133935568+Bjng05@users.noreply.github.com>
2024-02-08 21:58:19 +0800

    Create page.js

---
+++ src/app/news/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import NewsSection from "@/components/NewsSection";
+import CarouselSection from '@/components/CarouselSection'; 
+import Popular from "@/components/Popular";
+import Image from "next/image";
+import React from 'react';
+
+export default function Home() {
+	return (
+		<main>
+			<NewsSection />
+		</main>
+	);
+}
